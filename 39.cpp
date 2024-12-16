aecbe1d8786b5b5b85a162711345289ac21537f2
Shweta Shetty <shettyshweta48@gmail.com>
2024-12-16 11:24:47 +0530

    Add files via upload

---
+++ 39.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s1 = "Shweta";
+    string r = s1.substr(3, 2);
+    cout << "String is:" << r;
+    return 0;
+}
